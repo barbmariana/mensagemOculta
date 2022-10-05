00d4059fc2b23a7492217e6a26cd7e9681ad1c76
Programadores Cariocas 2022.1 <marianasouzaeco@gmail.com>
2022-10-05 11:59:21 -0300

    firstCommit

---
+++ script.js
@@ -0,0 +1,16 @@
+// var listaConvidados = ["mariana", "fabricio", "ana", "ruan"];
+// var contador = 0;
+
+// while(contador < listaConvidados.lenght) {
+//     console.log(listaConvidados[contador])
+//     contador++;
+// }
+// console.log("Lista de convidados chegou ao fim")
+
+// var feira = ['banana', 'pera', 'amora', 'morango', 'mamao']
+// feira= feira.splice(2,2);
+// console.log(feira);
+
+var feira = ['banana', 'pera', 'amora', 'morango', 'mamao'];
+feira[1]='leite';
+console.log(feira)
