002cf97af1957295e60873ba0744454d16643ba6
Programadores Cariocas 2022.1 <marianasouzaeco@gmail.com>
2022-10-07 09:24:08 -0300

    newComments

--- script.js
+++ script.js
@@ -11,6 +11,19 @@
 // feira= feira.splice(2,2);
 // console.log(feira);
 
-var feira = ['banana', 'pera', 'amora', 'morango', 'mamao'];
-feira[1]='leite';
-console.log(feira)
+// var feira = ['banana', 'pera', 'amora', 'morango', 'mamao'];
+// feira[1]='leite';
+// // console.log(feira)
+
+
+// var gastos = [
+//     [32.98, 17.3, 28.4, 55.2]
+//     [25.7, 17,13, 13.00]
+// ]
+
+// function somaARR(arr){
+//     var total=0
+//     for(var i=0; i<arr.lenght; i++){
+//         total+= arr[i];
+// }
+//     return total;
