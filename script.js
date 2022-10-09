d69b990a76cb54daa88e12a0eeb56e9651e15da3
Mariana Barbosa de Souza <barbmariana@users.noreply.github.com>
2022-10-09 18:59:50 -0300

    newOnclickChanges

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function exibir() {
-    document.getElementById("incremento").style.display = "block";
-}
+// // Função para sumir incremento
+
+// var cifraDeCesar = document.getElementById('radio-cifra')
+// var base64 = document.getElementById('radio-base64')
+// var incremento = document.getElementById('incremento')
+
+// function desaparecer(){
+//     if(cifraDeCesar.checked){
+//         incremento.style.display="inline"
+//     } else{
+//         incremento.style.display="none"
+//     }
+// }
+
+
+// // Função para trocar texto do botao
+
+// var botao = document.getElementById('botao');
+// var codificar = document.getElementById('codificador');
+// var decodificar = document.getElementById('decodificador');
