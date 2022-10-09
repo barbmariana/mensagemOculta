85fa028283a71945de6c84aad27dce4b7531e519
Mariana Barbosa de Souza <barbmariana@users.noreply.github.com>
2022-10-09 20:09:53 -0300

    newJSFunctions

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 // // Função para sumir incremento
 
-// var cifraDeCesar = document.getElementById('radio-cifra')
-// var base64 = document.getElementById('radio-base64')
-// var incremento = document.getElementById('incremento')
-
-// function desaparecer(){
-//     if(cifraDeCesar.checked){
-//         incremento.style.display="inline"
-//     } else{
-//         incremento.style.display="none"
-//     }
-// }
+function desaparecer(){
+var cifraDeCesar = document.getElementById('radio-cifra')
+var base64 = document.getElementById('radio-base64')
+var incremento = document.getElementById('incremento')
+    if(cifraDeCesar.checked){
+        incremento.style.display="inline"
+    } else if(base64.checked){
+        incremento.style.display="none"
+    }
+}
 
 
 // // Função para trocar texto do botao
 
-// var botao = document.getElementById('botao');
-// var codificar = document.getElementById('codificador');
-// var decodificar = document.getElementById('decodificador');
+function troca(){
+var botao = document.getElementById('botao');
+var codificar = document.getElementById('codificador');
+var decodificar = document.getElementById('decodificador');
+    if(decodificar.checked){
+        botao.innerText ='Decodificando'
+    } else if(codificar.checked){
+        botao.innerText='Codificando'
+    }
+}
+
+// Função para pegar mensagem inserida
+
+function mensagem(){
+    var mensagem = document.getElementById('message')
+    document.write.innerText(mensagem.value)
+}
