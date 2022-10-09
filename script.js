c17803c8aa0cfe7e12e9d3c366f31153c8cc51d5
Mariana Barbosa de Souza <barbmariana@users.noreply.github.com>
2022-10-09 12:11:20 -0300

    newJsButtonRemove

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-var metodo = document.getElementsById("metodo");
-var value = metodo.value;
-
-var incremento = document.getElementById("incremento");
-
-function desaparecer(){
-    if(value=="base64"){
-    incremento.style.display="hidden";
-}
-}
-
-function enviar(){
-    console.log(metodo)
+function exibir() {
+    document.getElementById("incremento").style.display = "block";
 }
